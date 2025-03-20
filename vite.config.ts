265fd54cf6cac9e53fa18c0b3b19c28d468e379e
zenith727 <zenith@nekos.work>
2025-03-20 23:12:34 +0700

    bruh

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+import lucidePreprocess from "vite-plugin-lucide-preprocess";
 // https://vitejs.dev/config/
 export default defineConfig({
   
-  plugins: [react()],
+  plugins: [react(),lucidePreprocess()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
